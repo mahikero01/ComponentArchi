7162d5c2848948d0f88fc26fe479abb972f5ca23
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2020-05-22 19:31:55 +0800

    up

--- ComponentArchi/Component/Result1Control.xaml.cs
+++ ComponentArchi/Component/Result1Control.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using ComponentArchi.Model;
+using System;
 using System.ComponentModel;
 using Windows.UI.Xaml.Controls;
+
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -36,5 +38,35 @@ namespace ComponentArchi.Component
                 PropertyChanged(this, new PropertyChangedEventArgs(name));
             }
         }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            
+            TimeSpan tes =  timePickme.Time;
+
+            CurrentTime = tes.ToString();
+
+            var test = "Rico";
+        }
+
+        private void Button_Click2(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            //TimeSpan temp = new TimeSpan(16,15,00);
+
+            //timePickme.Time = temp;
+
+           var test2  = TimeSpan.Parse(CurrentTime);
+
+            timePickme.Time = test2;
+          var test = "Rico";
+        }
+
+        private string _currentTime;
+        public string CurrentTime
+        { 
+            get { return  _currentTime; }
+            set { _currentTime = value;  }
+        }
+
     }
 }
